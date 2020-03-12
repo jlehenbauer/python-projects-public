df1a81cb99c13c049059d9a5259a985835e5d088
Jacob Lehenbauer <jake@lehenbauer.us>
2020-03-11 21:23:44 -0500

    Updated 4-2 to 100%

--- codility/4-2_MaxCounters.py
+++ codility/4-2_MaxCounters.py
@@ -1,12 +1,17 @@
 def solution(N, A):
-    uses = {x:0 for x in range(1, N+2)}
-    max = 0
+    solution = [0 for x in range(1, N+1)]
+    current_max = 0
+    add_max = 0
     for x in A:
         if x > N:
-            uses = {x:0 for x in range(1, N+2)}
-            uses[x] += max
-        else:
-            uses[x] += 1
-            if uses[x] > max:
-                max = uses[x]
-    return [uses[x] + uses[N+1] for x in range(1, N+1)]
+            add_max = current_max
+        elif solution[x-1] < add_max:
+            solution[x-1] = add_max
+        if x <= N:
+            solution[x-1] += 1
+            if solution[x-1] > current_max:
+                current_max = solution[x-1]
+    for i in range(len(solution)):
+        if solution[i] < add_max:
+            solution[i] = add_max
+    return solution
