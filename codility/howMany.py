3418bfb88e45e2427c2d05379d5385f8b3293129
Jacob Lehenbauer <jake@lehenbauer.us>
2020-03-18 13:40:34 -0500

    Update to howmany.py test

--- codility/howMany.py
+++ codility/howMany.py
@@ -1,15 +1,27 @@
 import sys
+import math
 
 def howMany(start, end, step):
-	start = int(start)
-	end = int(end)
-	step = int(step)
 	count = 0
 	for i in range(start, end):
 		if i % step == 0:
 			count += 1
-	print(count)
 	return count
 
+def solution(A, B, K):
+	new_list = list(range(1, B))[K+1::K]
+	return len([x for x in new_list if x >= A])
+
+def divide(A, B, K):
+	return int(round_half_up((B - A) / K))
+
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
+
+
 if __name__ == "__main__":
-	howMany(sys.argv[1], sys.argv[2], sys.argv[3])
+	a, b, c = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
+	print(howMany(a, b, c))
+	print(solution(a, b, c))
+	print(divide(a, b, c))
