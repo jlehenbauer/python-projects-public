577fa5adc12fab07493b3ca8674b71f9119b98dd
Jacob Lehenbauer <jake@lehenbauer.us>
2022-09-27 12:24:24 -0500

    random_group_maker TODO: while hang l40

--- random_group_maker.py
+++ random_group_maker.py
@@ -17,18 +17,27 @@ def main():
 
     already_assigned = {person:[] for person in people}
 
-    while len(already_assigned[list(already_assigned.keys())[0]]) < num_stations:
+    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 
-        print("Sending to first station . . .")
+    i = 1
+
+    while len(already_assigned[list(already_assigned.keys())[-1]]) < num_stations:
+
+        print(f"Sending to {ordinal(i)} station . . .")
+        i += 1
 
         already_assigned = stations(people, num_stations, already_assigned)
 
         input("Press enter to continue")
 
+    print("Done! Every student has been to every station:")
+    print(already_assigned)
+
 def stations(people, num_stations, already_assigned):
     assignments = {(x+1):[] for x in range(num_stations)}
     for person in people:
         assignment = random.randint(1, num_stations)
+        # TODO: hangs if current person cannot be assigned due to possible groups already being full
         while assignment in already_assigned[person] or len(assignments[assignment]) >= len(people)/num_stations:
             assignment = random.randint(1, num_stations)
         already_assigned[person].append(assignment)
